bcbfd4fc61666f6ad6668716d17c97bc9198b22f
Anwarul07 <mdanwarul064@gmail.com>
2024-04-29 23:44:20 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,12 @@
-console.log("digital clock")
+console.log("digital clock");
+let hrs=document.getElementById("hours");
+let min=document.getElementById("min");
+let sec=document.getElementById("sec");
+
+
+setInterval(() => {
+    hrs.innerHTML =(new Date().getHours()<10?"0":"")+new Date().getHours();
+    min.innerHTML= (new Date().getMinutes()<10?"0":"")+new Date().getMinutes();
+    sec.innerHTML= (new Date().getSeconds()<10?"0":"")+new Date().getSeconds();
+      
+}, 500);
